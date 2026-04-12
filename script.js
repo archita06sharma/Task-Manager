2762888930bc70e0d0020276fdb948d28f747049
archita06sharma <architasharma2006@gmail.com>
2026-04-12 11:45:41 +0530

    Added localStorage support for task persistence

--- script.js
+++ script.js
@@ -2,25 +2,49 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
 
+let tasks = [];
+
 button.addEventListener("click",function(){
     const taskText = input.value;
 
     if(taskText==="") return;
 
-    const li = document.createElement("li");
-    li.textContent = taskText;
+    tasks.push(taskText);
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText="Delete";
-    
-    deleteBtn.addEventListener("click",function(){
-        li.remove();
-    })
-    
-    list.appendChild(li);
-    li.appendChild(deleteBtn);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+
+    addTaskToUI(taskText);
 
     input.value = "";
 
 });
 
+function addTaskToUI(taskText){
+    const li = document.createElement("li");
+    li.textContent=taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    deleteBtn.addEventListener("click",function(){
+        li.remove();
+
+        tasks = tasks.filter(task => task !== taskText);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+    });
+
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+}
+
+window.addEventListener("load",function(){
+    const storedTasks=localStorage.getItem("tasks");
+
+    if(storedTasks){
+        tasks = JSON.parse(storedTasks);
+
+        tasks.forEach(task => {
+            addTaskToUI(task);
+        });
+    }
+});
